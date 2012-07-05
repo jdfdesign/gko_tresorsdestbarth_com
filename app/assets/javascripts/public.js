720c0df886d510c30a538b5789b22217931268ab
jdfdesign <regis@joufdesign.com>
2012-07-05 15:08:06 -0400

    bump 0.3.20

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -6,7 +6,7 @@
 //= require jquery.sidescroll.js
 //= require jquery.mousewheel.3.0.6
 //= require jquery.gridnav
-//= require galleria/galleria
+//= require gko/gko.galleria
 //= require history/scripts/bundled/html5/jquery.history.js
 $(document).ready(function() {
 
@@ -243,7 +243,85 @@ $(document).ready(function() {
 		}
 		else {
 			if($('.galleria').length > 0) {
-			    Galleria.loadTheme('/assets/galleria.classic.js');
+			 Galleria.addTheme({
+			        name:'classic',
+			        author:'Galleria',
+			        css:'galleria.classic.css',
+			        defaults:{
+			            transition:'slide',
+			            thumbCrop:'height',
+
+			            // set this to false if you want to show the caption all the time:
+			            _toggleInfo:false
+			        },
+			        init:function (options) {
+
+			            // add some elements
+			            this.addElement('info-link', 'info-close');
+			            this.append({
+			                'info':['info-link', 'info-close']
+			            });
+
+			            // cache some stuff
+			            var info = this.$('info-link,info-close,info-text'),
+			                touch = Galleria.TOUCH,
+			                click = touch ? 'touchstart' : 'click';
+
+			            // show loader & counter with opacity
+			            this.$('loader,counter').show().css('opacity', 0.4);
+
+			            // some stuff for non-touch browsers
+			            if (!touch) {
+			                this.addIdleState(this.get('image-nav-left'), { left:-50 });
+			                this.addIdleState(this.get('image-nav-right'), { right:-50 });
+			                this.addIdleState(this.get('counter'), { opacity:0 });
+			            }
+
+			            // toggle info
+			            if (options._toggleInfo === true) {
+			                info.bind(click, function () {
+			                    info.toggle();
+			                });
+			            } else {
+			                info.show();
+			                this.$('info-link, info-close').hide();
+			            }
+
+			            // bind some stuff
+			            this.bind('thumbnail', function (e) {
+
+			                if (!touch) {
+			                    // fade thumbnails
+			                    $(e.thumbTarget).css('opacity', 0.6).parent().hover(function () {
+			                        $(this).not('.active').children().stop().fadeTo(100, 1);
+			                    }, function () {
+			                        $(this).not('.active').children().stop().fadeTo(400, 0.6);
+			                    });
+
+			                    if (e.index === this.getIndex()) {
+			                        $(e.thumbTarget).css('opacity', 1);
+			                    }
+			                } else {
+			                    $(e.thumbTarget).css('opacity', this.getIndex() ? 1 : 0.6);
+			                }
+			            });
+
+			            this.bind('loadstart', function (e) {
+			                if (!e.cached) {
+			                    this.$('loader').show().fadeTo(200, 0.4);
+			                }
+
+			                this.$('info').toggle(this.hasInfo());
+
+			                $(e.thumbTarget).css('opacity', 1).parent().siblings().children().css('opacity', 0.6);
+			            });
+
+			            this.bind('loadfinish', function (e) {
+			                this.$('loader').fadeOut(200);
+			            });
+			        }
+			    });
+
 			    Galleria.run('.galleria', {
 					autoplay: true,
 					responsive: true,
